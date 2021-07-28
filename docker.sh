fcae269eada1706699a906cbeb3cb44846a84510
Pixwok <40206880+Pixwok@users.noreply.github.com>
2021-07-28 11:19:56 +0200

    Docker stable

--- docker.sh
+++ docker.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-
+echo "Quel utilisateur doit utiliser Docker: "
+read user
 echo "Lancement Installation de Docker "
 sleep 1
+#Install Docker
+curl -sSL get.docker.com | sh
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key   fingerprint   0EBFCD88
-
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-apt update
-
-#Installation de docker et docker-compose
-apt install docker-ce docker-ce-cli containerd.io
-
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+usermod -aG docker $user
